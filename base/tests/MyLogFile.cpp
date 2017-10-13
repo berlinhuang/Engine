d441a8e1bddeed5581ff31abb5a2b34ed5302b97
berlinhuang <berlinmail@qq.com>
2017-10-13 10:25:27 +0800

    complete MyLogFile.cpp and MyFileUtil.cpp

--- base/tests/MyLogFile.cpp
+++ base/tests/MyLogFile.cpp
@@ -5,16 +5,24 @@
 #include "./../LogFile.h"
 #include "./../Logging.h"
 
-boost::scoped_ptr<LogFile> g_logFile;
+boost::scoped_ptr<LogFile> g_logFile;//全局变量，输出到这里
 
 void outputFunc(const char* msg, int len)
 {
     g_logFile->append(msg, len);
+    // scoped_ptr<T> 重载operator->，
+    // 调用LogFile::append(),
+    // 间接调用File::append();
+    // 最后 ::fwrite_unlocked(fp_);
 }
 
 void flushFunc()
 {
     g_logFile->flush();
+    // scoped_ptr<T> 重载operator->，
+    // 调用LogFile::flush(),
+    // 间接调用File::flush()，
+    // 最后::fflush(fp_);
 }
 
 
@@ -28,7 +36,9 @@ int main(int argc, char* argv[])
     string line = "1234567890 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
     for(int i=0;i<10000;++i)
     {
+        // ./Engine/cmake-build-debug/base/tests/CMakeFiles/MylogFile.20171013-020448.berlin-virtual-machine.16353.log
         LOG_INFO<<line<<i;
+        // 不断地构造匿名的Logger对象，在~Logger()中调用dummyOutput，将日志信息写入文件
         usleep(1000);
     }
 
