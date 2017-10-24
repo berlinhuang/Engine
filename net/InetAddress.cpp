fd36131bb73309bb240ad0a394b7fd5805768673
berlinhuang <berlinmail@qq.com>
2017-10-24 17:57:14 +0800

    add net class InetAddress

--- net/InetAddress.cpp
+++ net/InetAddress.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "InetAddress.h"
+
+#include <boost/static_assert.hpp>
+
+InetAddress::InetAddress(uint16_t port, bool loopbackOnly, bool ipv6)
+{
+    //offsetof(a,b)第一个参数是结构体的名字，第二个参数是结构体成员的名字 该宏返回结构体structName中成员memberName的偏移量。偏移量是size_t类型的
+    BOOST_STATIC_ASSERT(offsetof(InetAddress, addr6_) == 0);
+}
--- net/InetAddress.h
+++ net/InetAddress.h
@@ -6,8 +6,38 @@
 #define ENGINE_INETADDRESS_H
 
 
-class InetAddress {
+#include <cstdint>
+#include "./../base/StringPiece.h"
+#include <netinet/in.h>
+namespace sockets
+{
+    const struct sockaddr* sockaddr_cast(const struct sockaddr_in6* addr);
+}
 
+
+
+class InetAddress {
+public:
+    explicit InetAddress(uint16_t port = 0, bool loopbackOnly = false, bool ipv6 = false );
+    InetAddress(StringArg ip, uint16_t, bool ipv6 = false);
+
+    explicit InetAddress(const struct sockaddr_in& addr):addr_(addr){}
+    explicit InetAddress(const struct sockaddr_in6& addr6):addr6_(addr6){}
+
+    const struct sockaddr* getSockAddr() const { return sockets::sockaddr_cast(&addr6_);}
+    void setSockAddrInet6(const struct sockaddr_in6& addr6){ addr6_ = addr6; }
+
+    uint32_t ipNetEndian() const;
+    uint16_t portNetEndian() const { return addr_.sin_port;}
+
+    static bool resolve(StringArg hostname, InetAddress* result);
+
+private:
+    union
+    {
+        struct sockaddr_in addr_;
+        struct sockaddr_in6 addr6_;
+    };
 };
 
 
