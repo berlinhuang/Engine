0b6e38966a204a855b81a1332d2cfc1ce6848b4d
berlinhuang <berlinmail@qq.com>
2017-11-14 20:29:05 +0800

    add EventLoop class

---
+++ net/Channel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 17-11-14.
+//
+
+#include "Channel.h"
---
+++ net/Channel.h
@@ -0,0 +1,19 @@
+//
+// Created by root on 17-11-14.
+//
+
+#ifndef ENGINE_CHANNEL_H
+#define ENGINE_CHANNEL_H
+
+class EventLoop;
+
+
+class Channel {
+public:
+    
+
+
+};
+
+
+#endif //ENGINE_CHANNEL_H
