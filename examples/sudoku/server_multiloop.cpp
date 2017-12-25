885ce2cc4904e88d3397676b2d63899447534e4c
berlinhuang <berlinmail@qq.com>
2017-12-25 10:23:27 +0800

    add sudoku

--- examples/sudoku/server_multiloop.cpp
+++ examples/sudoku/server_multiloop.cpp
@@ -1,8 +1,3 @@
-/**
- * server_basic.cpp 是一个并发服务器，可以同时服务多个客户连接。但是它是单线程的，无法发挥多核硬件的能力。
- * 适合 IO 密集的应用，不太适合 CPU 密集的应用，因为较难发挥多核的威力。
- */
-
 #include "sudoku.h"
 
 #include "../../base/Atomic.h"
@@ -19,19 +14,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 class SudokuServer
 {
  public:
-  SudokuServer(EventLoop* loop, const InetAddress& listenAddr)
+  SudokuServer(EventLoop* loop, const InetAddress& listenAddr, int numThreads)
     : server_(loop, listenAddr, "SudokuServer"),
+      numThreads_(numThreads),
       startTime_(Timestamp::now())
   {
-    server_.setConnectionCallback( boost::bind(&SudokuServer::onConnection, this, _1));
-    server_.setMessageCallback( boost::bind(&SudokuServer::onMessage, this, _1, _2, _3));
+    server_.setConnectionCallback(
+        boost::bind(&SudokuServer::onConnection, this, _1));
+    server_.setMessageCallback(
+        boost::bind(&SudokuServer::onMessage, this, _1, _2, _3));
+    server_.setThreadNum(numThreads);
   }
 
   void start()
   {
+    LOG_INFO << "starting " << numThreads_ << " threads.";
     server_.start();
   }
 
@@ -42,11 +43,7 @@ class SudokuServer
         << conn->localAddress().toIpPort() << " is "
         << (conn->connected() ? "UP" : "DOWN");
   }
-  /**
-   * 处理协议格式，并调用 solveSudoku() 求解问题
-   * @param conn
-   * @param buf
-   */
+
   void onMessage(const TcpConnectionPtr& conn, Buffer* buf, Timestamp)
   {
     LOG_DEBUG << conn->name();
@@ -59,7 +56,7 @@ class SudokuServer
         string request(buf->peek(), crlf);
         buf->retrieveUntil(crlf + 2);
         len = buf->readableBytes();
-        if (!processRequest(conn, request)) // call solveSudoku()
+        if (!processRequest(conn, request))
         {
           conn->send("Bad Request!\r\n");
           conn->shutdown();
@@ -117,15 +114,21 @@ class SudokuServer
   }
 
   TcpServer server_;
+  int numThreads_;
   Timestamp startTime_;
 };
 
 int main(int argc, char* argv[])
 {
   LOG_INFO << "pid = " << getpid() << ", tid = " << CurrentThread::tid();
+  int numThreads = 0;
+  if (argc > 1)
+  {
+    numThreads = atoi(argv[1]);
+  }
   EventLoop loop;
   InetAddress listenAddr(9981);
-  SudokuServer server(&loop, listenAddr);
+  SudokuServer server(&loop, listenAddr, numThreads);
 
   server.start();
 
